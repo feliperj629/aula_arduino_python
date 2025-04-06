2e221a36aef3cec04653ce3fd49e41e2a633222c
feliperj629 <feliperj629@gmail.com>
2025-04-06 11:31:04 -0300

    Incluindo um print para acompalhar execução.

--- led_firmata.py
+++ led_firmata.py
@@ -7,9 +7,11 @@ board = Arduino('COM4')  # Para Windows
 
 # Configura o pino 13 como saída (LED embutido no Arduino Uno)
 led = board.get_pin('d:13:o')
-
+i=0
 while True:
     led.write(1)  # Liga o LED
-    time.sleep(2)
+    time.sleep(1)
     led.write(0)  # Desliga o LED
-    time.sleep(2)
+    time.sleep(1)
+    print(f"Execução: {i}")
+    i+=1
